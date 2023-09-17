c07a80aa24f403fbf88cd6938179adb8696ab09c
JoneyPereira <38810506+JoneyPereira@users.noreply.github.com>
2023-09-17 09:32:40 -0300

    Ajuste no objeto Usuario

--- desafio.kt
+++ desafio.kt
@@ -1,10 +1,10 @@
 // [Template no Kotlin Playground](https://pl.kotl.in/WcteahpyN)
 
-enum class Nivel { BASICO, INTERMEDIARIO, DIFICIL }
+enum class Nivel { BASICO, INTERMEDIARIO, AVANCADO }
 
-class Usuario(var nome: String)
+data class Usuario(val nome: String)
 
-data class ConteudoEducacional(var nome: String, val duracao: Int = 60)
+data class ConteudoEducacional(val nome: String, val duracao: Int)
 
 data class Formacao(val nome: String, val nivel: Nivel, var conteudos: List<ConteudoEducacional>) {
 
@@ -19,13 +19,16 @@ data class Formacao(val nome: String, val nivel: Nivel, var conteudos: List<Cont
 fun main() {
     //TODO("Analise as classes modeladas para este domínio de aplicação e pense em formas de evoluí-las.")
     //TODO("Simule alguns cenários de teste. Para isso, crie alguns objetos usando as classes em questão.")
-    val usuario = Usuario("Joney")
+    val joney = Usuario("Joney")
+    val pedro = Usuario("Pedro")
     val java = ConteudoEducacional("Java", 60)
     val kotlin = ConteudoEducacional("Kotlin", 60)
     val android = ConteudoEducacional("Android", 60)
     val conteudos = listOf(java, kotlin, android)
     val nivel = Nivel.BASICO
     val formacao = Formacao("Java Basico", nivel, conteudos)
-    formacao.matricular(usuario)
+    formacao.matricular(joney)
+    formacao.matricular(pedro)
     println(formacao)
+    println(formacao.inscritos)
 }
